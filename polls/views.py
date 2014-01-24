38a54aa1affca09a2cab69aa27fde94838edc89e
ralf <ovais_it@hotmail.com>
2014-01-24 09:40:48 +0100

    show 4 pols

--- polls/views.py
+++ polls/views.py
@@ -16,7 +16,7 @@ class IndexView(generic.ListView):
            published in the future).
         """
 
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date’)[:4]
 
 
 
